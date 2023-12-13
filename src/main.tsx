dfc25bcf25e08b0fe65c1d72e1257c00ec24a2c9
pllinas <pllinas42@gmail.com>
2023-12-13 20:32:38 +0100

    feat: :sparkles: Implementación React Router Dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+// LIBRARY IMPORTS
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// LOCAL IMPORTS
+import { Pokemon } from './pages/pokemon/pokemon.tsx';
+// STYLES IMPORTS
+import './index.css';
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Pokemon />,
+    }
+  ],
+);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
