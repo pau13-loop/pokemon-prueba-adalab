5af4f6030ff438c800d56e0b9f0cdf693f4a6f78
pllinas <pllinas42@gmail.com>
2023-12-16 23:21:29 +0100

    feat: :sparkles: Generamos el componente Spinner

---
+++ src/components/spinner/spinner.tsx
@@ -0,0 +1,11 @@
+import './styles.scss';
+
+const Spinner = () => {
+  return (
+    <div className="w-100 flexRow flexCenter">
+      <span className="loader" />
+    </div>
+  );
+};
+
+export { Spinner };
